979695706b6bc43b93cbe90b35c95badd356deb1
TripleNine666 <kolumb1709@gmail.com>
2023-02-02 18:15:57 +0300

    teleport dialog window to body

--- src/components/ManageGoals.vue
+++ src/components/ManageGoals.vue
@@ -2,11 +2,15 @@
   <h2>Manage Goals</h2>
   <input type="text" ref="goal" />
   <button @click="addGoal">Add goal</button>
-  <error-alert v-if="inputIsInvalid">
-    <h2>Input is invalid!</h2>
-    <p>Plase make sure that input is valid! May be input some characters...</p>
-    <button @click="confirmError">OK!</button>
-  </error-alert>
+  <teleport to="body">
+    <error-alert v-if="inputIsInvalid">
+      <h2>Input is invalid!</h2>
+      <p>
+        Plase make sure that input is valid! May be input some characters...
+      </p>
+      <button @click="confirmError">OK!</button>
+    </error-alert>
+  </teleport>
 </template>
 
 <script>
