52a1ae20f482e3ef7a347755e8555b07f9a3a195
TripleNine666 <kolumb1709@gmail.com>
2023-02-02 14:24:46 +0300

    first commit dive into components

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,43 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <the-header></the-header>
+    <badge-list></badge-list>
+    <user-info
+      :full-name="activeUser.name"
+      :info-text="activeUser.description"
+      :role="activeUser.role"
+    ></user-info>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import BadgeList from "./components/BadgeList.vue";
+import TheHeader from "./components/TheHeader.vue";
+import UserInfo from "./components/UserInfo.vue";
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    "the-header": TheHeader,
+    BadgeList: BadgeList,
+    UserInfo,
+  },
+  data() {
+    return {
+      activeUser: {
+        name: "Maximilian Schwarzmüller",
+        description: "Site owner and admin",
+        role: "admin",
+      },
+    };
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
 }
 </style>
---
+++ src/components/UserInfo.vue
@@ -0,0 +1,25 @@
+<template>
+  <base-card>
+    <header>
+      <h3>{{ fullName }}</h3>
+      <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
+    </header>
+    <p>{{ infoText }}</p>
+  </base-card>
+</template>
+
+<script>
+import BaseCard from "./BaseCard.vue";
+export default {
+  components: { BaseCard },
+  props: ["fullName", "infoText", "role"],
+};
+</script>
+
+<style scoped>
+section header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
