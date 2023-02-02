3d9db8cc3114c86854a3896751f9e9e74c3f0afc
TripleNine666 <kolumb1709@gmail.com>
2023-02-02 15:44:58 +0300

    scoped slots

--- src/App.vue
+++ src/App.vue
@@ -7,18 +7,25 @@
       :info-text="activeUser.description"
       :role="activeUser.role"
     ></user-info>
+    <course-goals #default="slotProps">
+      <h2>{{ slotProps.item }}</h2>
+      <p>{{ slotProps.text }}</p>
+    </course-goals>
   </div>
 </template>
 
 <script>
 import BadgeList from "./components/BadgeList.vue";
+import CourseGoals from "./components/CourseGoals.vue";
 import TheHeader from "./components/TheHeader.vue";
 import UserInfo from "./components/UserInfo.vue";
+
 export default {
   components: {
     "the-header": TheHeader,
     BadgeList: BadgeList,
     UserInfo,
+    CourseGoals,
   },
   data() {
     return {
