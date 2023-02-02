dcdc62a2c5c4034492a83e65d30897ea58cbd21d
TripleNine666 <kolumb1709@gmail.com>
2023-02-02 16:48:20 +0300

    using dialog window as component

--- src/components/ManageGoals.vue
+++ src/components/ManageGoals.vue
@@ -1,8 +1,36 @@
 <template>
   <h2>Manage Goals</h2>
-  <input type="text" />
+  <input type="text" ref="goal" />
+  <button @click="addGoal">Add goal</button>
+  <error-alert v-if="inputIsInvalid">
+    <h2>Input is invalid!</h2>
+    <p>Plase make sure that input is valid! May be input some characters...</p>
+    <button @click="confirmError">OK!</button>
+  </error-alert>
 </template>
 
 <script>
-export default {};
+import ErrorAlert from "./ErrorAlert.vue";
+
+export default {
+  components: {
+    ErrorAlert,
+  },
+  data() {
+    return {
+      inputIsInvalid: false,
+    };
+  },
+  methods: {
+    addGoal() {
+      const enteredValue = this.$refs.goal.value;
+      if (enteredValue === "") {
+        this.inputIsInvalid = true;
+      }
+    },
+    confirmError() {
+      this.inputIsInvalid = false;
+    },
+  },
+};
 </script>
