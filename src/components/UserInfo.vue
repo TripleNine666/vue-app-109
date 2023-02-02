13f9078e5b84ecb9ca7fb40cede4ccdfb12b2eb5
TripleNine666 <kolumb1709@gmail.com>
2023-02-02 15:07:49 +0300

    v-slot (#)

--- src/components/UserInfo.vue
+++ src/components/UserInfo.vue
@@ -1,11 +1,15 @@
 <template>
-  <base-card>
-    <header>
-      <h3>{{ fullName }}</h3>
-      <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
-    </header>
-    <p>{{ infoText }}</p>
-  </base-card>
+  <section>
+    <base-card>
+      <template #header>
+        <h3>{{ fullName }}</h3>
+        <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
+      </template>
+      <template #default>
+        <p>{{ infoText }}</p>
+      </template>
+    </base-card>
+  </section>
 </template>
 
 <script>
@@ -15,11 +19,3 @@ export default {
   props: ["fullName", "infoText", "role"],
 };
 </script>
-
-<style scoped>
-section header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
-</style>
